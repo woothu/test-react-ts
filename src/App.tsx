c4d42255b97f53e77267fc7724084ecd1d627db2
woothu <32914662+woothu@users.noreply.github.com>
2020-05-15 19:06:32 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,252 @@
+import React from 'react';
+import './App.css';
+import { 
+  CAlert,
+  CBadge,
+  CHeaderBrand,
+  CNavbarBrand,
+  CSidebarBrand,
+  CBreadcrumb,
+  CBreadcrumbItem,
+  CBreadcrumbRouter,
+  CButton,
+  CButtonClose,
+  CButtonGroup,
+  CButtonToolbar,
+  CCallout,
+  CCard,
+  CCardBody,
+  CCardFooter,
+  CCardGroup,
+  CCardHeader,
+  CCardImg,
+  CCardImgOverlay,
+  CCardSubtitle,
+  CCardText,
+  CCardTitle,
+  CCarousel,
+  CCarouselCaption,
+  CCarouselControl,
+  CCarouselIndicators,
+  CCarouselInner,
+  CCarouselItem,
+  CCol,
+  CCollapse,
+  CContainer,
+  CCreateElement,
+  CDataTable,
+  CDropdown,
+  CDropdownItem,
+  CDropdownMenu,
+  CDropdownToggle,
+  CElementCover,
+  CEmbed,
+  CEmbedItem,
+  CFade,
+  CFooter,
+  CForm,
+  CValidFeedback,
+  CInvalidFeedback,
+  CFormGroup,
+  CFormText,
+  CHeader,
+  CHeaderNav,
+  CHeaderNavItem,
+  CHeaderNavLink,
+  CImg,
+  CInput,
+  CTextarea,
+  CInputFile,
+  CInputCheckbox,
+  CInputRadio,
+  CSelect,
+  CInputGroup,
+  CInputGroupAppend,
+  CInputGroupPrepend,
+  CInputGroupText,
+  CJumbotron,
+  CLabel,
+  CLink,
+  CCardLink,
+  CListGroup,
+  CListGroupItemHeading,
+  CListGroupItem,
+  CListGroupItemText,
+  CMedia,
+  CMediaBody,
+  CModal,
+  CModalBody,
+  CModalFooter,
+  CModalHeader,
+  CModalTitle,
+  CNav,
+  CNavbar,
+  CNavbarNav,
+  CNavbarText,
+  CNavItem,
+  CNavLink,
+  CPagination,
+  CPopover,
+  CProgress,
+  CProgressBar,
+  CRow,
+  CSidebar,
+  CSidebarFooter,
+  CSidebarForm,
+  CSidebarHeader,
+  CSidebarMinimizer,
+  CSidebarNav,
+  CSidebarNavDivider,
+  // CSidebarNavDropdown,
+  CSidebarNavItem,
+  CSidebarNavTitle,
+  CSpinner,
+  CSubheader,
+  CTabContent,
+  CTabPane,
+  CTabs,
+  CToast,
+  CToastBody,
+  CToaster,
+  CToastHeader,
+  CTooltip,
+  CToggler,
+  CWidgetBrand,
+  CWidgetDropdown,
+  CWidgetIcon,
+  CWidgetProgress,
+  CWidgetProgressIcon,
+  CWidgetSimple
+} from '@coreui/react'
+
+function App() {
+  return (
+    <div className="App">
+      <CAlert>Content</CAlert>
+      <CBadge>Content</CBadge>
+      <CHeaderBrand>Content</CHeaderBrand>
+      <CNavbarBrand>Content</CNavbarBrand>
+      <CSidebarBrand>Content</CSidebarBrand>
+      <CBreadcrumb>Content</CBreadcrumb>
+      <CBreadcrumbItem>Content</CBreadcrumbItem>
+      <CBreadcrumbRouter/>
+      <CButton>Content</CButton>
+      <CButtonClose>Content</CButtonClose>
+      <CButtonGroup>Content</CButtonGroup>
+      <CButtonToolbar>Content</CButtonToolbar>
+      <CCallout>Content</CCallout>
+      <CCard>Content</CCard>
+      <CCardBody>Content</CCardBody>
+      <CCardFooter>Content</CCardFooter>
+      <CCardGroup>Content</CCardGroup>
+      <CCardHeader>Content</CCardHeader>
+      <CCardImg />
+      <CCardImgOverlay>Content</CCardImgOverlay>
+      <CCardSubtitle>Content</CCardSubtitle>
+      <CCardText>Content</CCardText>
+      <CCardTitle>Content</CCardTitle>
+      <CCarousel>
+        <CCarouselCaption>Content</CCarouselCaption>
+        <CCarouselControl direction="next">Content</CCarouselControl>
+        <CCarouselIndicators />
+        <CCarouselInner>Content</CCarouselInner>
+        <CCarouselItem>Content</CCarouselItem>
+      </CCarousel>
+      <CCol>Content</CCol>
+      <CCollapse>Content</CCollapse>
+      <CContainer>Content</CContainer>
+      <CCreateElement items={[]} />
+      <CDataTable />
+      <CDropdown>
+        <CDropdownItem>Content</CDropdownItem>
+        <CDropdownMenu>Content</CDropdownMenu>
+        <CDropdownToggle>Content</CDropdownToggle>
+      </CDropdown>
+      <CElementCover>Content</CElementCover>
+      <CEmbed>Content</CEmbed>
+      <CEmbedItem>Content</CEmbedItem>
+      <CFade>Content</CFade>
+      <CFooter>Content</CFooter>
+      <CForm>Content</CForm>
+      <CValidFeedback>Content</CValidFeedback>
+      <CInvalidFeedback>Content</CInvalidFeedback>
+      <CFormGroup>Content</CFormGroup>
+      <CFormText>Content</CFormText>
+      <CHeader>Content</CHeader>
+      <CHeaderNav>Content</CHeaderNav>
+      <CHeaderNavItem>Content</CHeaderNavItem>
+      <CHeaderNavLink>Content</CHeaderNavLink>
+      <CImg />
+      <CInput />
+      <CTextarea />
+      <CInputFile />
+      <CInputCheckbox />
+      <CInputRadio />
+      <CSelect>Content</CSelect>
+      <CInputGroup>Content</CInputGroup>
+      <CInputGroupAppend>Content</CInputGroupAppend>
+      <CInputGroupPrepend>Content</CInputGroupPrepend>
+      <CInputGroupText>Content</CInputGroupText>
+      <CJumbotron>Content</CJumbotron>
+      <CLabel>Content</CLabel>
+      <CLink>Content</CLink>
+      <CCardLink>Content</CCardLink>
+      <CListGroup>Content</CListGroup>
+      <CListGroupItemHeading>Content</CListGroupItemHeading>
+      <CListGroupItem>Content</CListGroupItem>
+      <CListGroupItemText>Content</CListGroupItemText>
+      <CMedia>Content</CMedia>
+      <CMediaBody>Content</CMediaBody>
+      <CModal>Content</CModal>
+      <CModalBody>Content</CModalBody>
+      <CModalFooter>Content</CModalFooter>
+      <CModalHeader>Content</CModalHeader>
+      <CModalTitle>Content</CModalTitle>
+      <CNav>Content</CNav>
+      <CNavbar>Content</CNavbar>
+      <CNavbarNav>Content</CNavbarNav>
+      <CNavbarText>Content</CNavbarText>
+      <CNavItem>Content</CNavItem>
+      <CNavLink>Content</CNavLink>
+      <CPagination onActivePageChange={() => {}} />
+      <CPopover><div>Content</div></CPopover>
+      <CProgress>Content</CProgress>
+      <CProgressBar>Content</CProgressBar>
+      <CRow>Content</CRow>
+      <CSidebar>Content</CSidebar>
+      <CSidebarFooter>Content</CSidebarFooter>
+      <CSidebarForm>Content</CSidebarForm>
+      <CSidebarHeader>Content</CSidebarHeader>
+      <CSidebarMinimizer />
+      <CSidebarNav>Content</CSidebarNav>
+      <CSidebarNavDivider />
+      {/* <CSidebarNavDropdown>Content</CSidebarNavDropdown> */}
+      <CSidebarNavItem>Content</CSidebarNavItem>
+      <CSidebarNavTitle>Content</CSidebarNavTitle>
+      <CSpinner />
+      <CSubheader>Content</CSubheader>
+      <CTabContent>Content</CTabContent>
+      <CTabPane>Content</CTabPane>
+      <CTabs>Content</CTabs>
+      <CToaster>
+        <CToast show>
+          <CToastHeader>Content</CToastHeader>
+          <CToastBody>Content</CToastBody>
+        </CToast>
+      </CToaster>
+
+      <CTooltip>
+        <div>Tooltip</div>
+      </CTooltip>
+      <CToggler>Content</CToggler>
+      <CWidgetBrand>Content</CWidgetBrand>
+      <CWidgetDropdown>Content</CWidgetDropdown>
+      <CWidgetIcon>Content</CWidgetIcon>
+      <CWidgetProgress>Content</CWidgetProgress>
+      <CWidgetProgressIcon>Content</CWidgetProgressIcon>
+      <CWidgetSimple>Content</CWidgetSimple>
+    </div>
+  );
+}
+
+export default App;

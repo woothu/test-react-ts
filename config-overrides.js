506d1a2f609d7920386b9c33c6ad95b55c3fc889
woothu <32914662+woothu@users.noreply.github.com>
2020-05-21 12:21:07 +0200

    fix: add aliases allowing npm link

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,7 @@
 const {
   override,
-  babelInclude
+  babelInclude,
+  addWebpackAlias
 } = require('customize-cra');
 const path = require('path');
 const fs = require('fs');
@@ -9,5 +10,9 @@ module.exports = override(
   babelInclude([
     path.resolve('src'), // make sure you link your own source
     fs.realpathSync('node_modules/@coreui/react')
-  ])
+  ]),
+  addWebpackAlias({
+    react: path.resolve('./node_modules/react'),
+    'react-dom': path.resolve('./node_modules/react-dom')
+  })
 );
